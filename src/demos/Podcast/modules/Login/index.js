907fa49575f6b39c2f61b56cfb2ba0e480822021
Hamza Khurshid <hamza.khurshid96@gmail.com>
2022-08-11 17:49:40 +0500

    changes

--- src/demos/Podcast/modules/Login/index.js
+++ src/demos/Podcast/modules/Login/index.js
@@ -1,11 +1,11 @@
 import { Button, Input } from '../../../sharedComponents';
 import { ReactComponent as Mic } from './assets/mic.svg'
-import { ReactComponent as Logo } from '../../../../logo.svg'
+import { ReactComponent as AppFlowsLogo } from '../../../sharedComponents/assets/AppFlowsLogo.svg';
 
 const Login = () => {
   return(
     <div className="bg-[#E7F4F2] h-screen flex flex-col items-center pt-14">
-      <Logo/>
+      <AppFlowsLogo />
       <div className="w-[680px] h-[600px] bg-white flex flex-col justify-center items-center mt-14">
         <Mic/>
         <div className='text-3xl font-bold my-12 '>The Podcast App</div>
